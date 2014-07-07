9a97f1979184a3406c86f9e32b28ba92b76b1c72
xanpeng <xanpeng@gmail.com>
2014-07-07 14:11:43 +0800

    add example: boost program_options

---
+++ examples/boost-snippets/program-options.cc
@@ -0,0 +1,35 @@
+//
+// g++ program-options.cc -o program-options -lboost_program_options
+//
+
+#include <boost/program_options.hpp>
+#include <iostream>
+
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[]) {
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "show this help message")
+      ("compression", po::value<int>()->required(), "set compression level");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 1;
+    }
+    if (vm.count("compression")) {
+      std::cout << "compression level was " << vm["compression"].as<int>() << std::endl;
+    }
+  }
+  catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
